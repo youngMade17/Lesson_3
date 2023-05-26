5102a0d8638a612f44237efabaaab4a4723151d3
Mikhail Gundenkov_idea <gundenkov.99@gmail.com>
2023-05-26 19:16:54 +0300

    writing test fot test-box form

---
+++ src/test/java/fillForm.java
@@ -0,0 +1,35 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class fillForm {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    static {
+        Configuration.pageLoadStrategy = "eager";
+    }
+    @Test
+    void fillFormTest() {
+        open("/text-box");
+
+        $("#userName").setValue("Alex Egorov");
+
+        $("#userName").setValue("Alex Egorov");
+        $("#userEmail").setValue("alex@egorov.com");
+        $("#currentAddress").setValue("Some address 1");
+        $("#permanentAddress").setValue("Another address 1");
+        $("#submit").click();
+
+        $("#output").shouldHave(text("Alex Egorov"), text("alex@egorov.com"),
+                text("Some address 1"), text("Another address 1"));
+    }
+}
